a6c120977962a3f5d100255281e12be52c39a836
markquiba <markjquiba@gmail.com>
2018-10-11 04:01:38 +0800

    Update rendered articles

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,40 +1,104 @@
 <template>
-  <v-container fluid grid-list-sm>
+  <v-container fluid grid-list-xs>
     <div class="top-toolbar">
       <h1 class="head-title">Headlines</h1>
-      <v-btn class="filter-btn" color="info">Filter</v-btn>
+      <v-select
+        v-model="selectedSource"
+        class="filter-btn"
+        :items="sourceNames"
+        label="Filter by Source"
+        outline
+      ></v-select>
     </div>
-    <v-layout row wrap class="news">
-      <v-flex v-for="(article, index) in articles" :key="index" xs3>
-        <div>
-          {{ article.title }}
+    <v-layout row wrap class="news-wrapper">
+      <v-flex v-for="(article, index) in articles" :key="index" xs12 sm6 md4>
+        <div class="article-wrapper">
+          <div :v-if="article.urlToImage != null" class="article" :style="`background-image: url(${ (article.urlToImage != null) ? article.urlToImage : 'https://media.istockphoto.com/photos/abstract-blurred-background-image-with-beautiful-colors-picture-id501776098?k=6&m=501776098&s=612x612&w=0&h=9nGtVlPb6Cedv-T9G6h5U5kLD_goVfUcvQ1G1nb6lJk='});`"></div>
+          <div class="content">
+              <span>{{ article.publishedAt | moment("DD.MM.YY") }}</span>
+              <h5>{{ article.title }}</h5>
+              <small>{{ article.content }}</small>
+            </div>
         </div>
+
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 
 export default {
+  data() {
+    return {
+      selectedSource: ''
+    }
+  },
   computed: {
-    ...mapState([
-      'articles'
-    ])
+    articles() {
+      return this.$store.state.articles
+    },
+    sourcesIds() {
+      return this.$store.getters.getSourceIds
+    },
+    sourceNames() {
+      return this.$store.getters.getSourceNames
+    }
+  },
+  methods: {
+    filter(selectedSource) {
+      console.log(selectedSource)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.article {
+	width: 100%;
+	height: 300px;
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-size: cover;
+  border-radius: 5px;
+  transition: 0.3s ease-in;
+}
+
+.img-filter {
+  position: relative;
+}
+
+.article .content {
+  position: relative;
+  bottom: 0;
+  padding: 20px;
+}
+
+.search {
+  margin-top: 50px;
+}
+
 .filter-btn {
   float: right;
+  padding: 0;
+  margin: 0;
 }
 .head-title {
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 3.6em;
+  color:#5C6BC0;
   float: left;
 }
 .top-toolbar {
   overflow: hidden;
+}
+.news-wrapper {
+  margin-top: -15px !important;
+}
+@media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+  .head-title {
+    display: none;
+  }
 }
 </style>
