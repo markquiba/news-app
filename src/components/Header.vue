d457c9d10d3bcb4b55a60a54d876c5fd4bc8519e
markquiba <markjquiba@gmail.com>
2018-10-11 05:15:41 +0800

    Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="header-title">{{ headerTitle }}</h1>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState([
+      'headerTitle'
+    ])
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header-title {
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-size: 3.6em;
+  color:#5C6BC0;
+  float: left;
+}
+</style>
