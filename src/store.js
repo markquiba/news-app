facad8ce085e018d51e30a27da937b1466a4313c
markquiba <markjquiba@gmail.com>
2018-10-11 07:31:32 +0800

    Call laoder action commits

--- src/store.js
+++ src/store.js
@@ -33,37 +33,49 @@ export default new Vuex.Store({
     },
     FILTER_ARTICLES(state, articles) {
       state.articles = articles
+    },
+    ON_LOADER(state) {
+      state.loader = true
+    },
+    OFF_LOADER(state) {
+      state.loader = false
     }
   },
   actions: {
     fetchArticles({ commit }, params) {
+      commit("ON_LOADER");
       return new Promise((resolve, reject) => {
         fetch(`https://newsapi.org/v2/top-headlines?country=${params.country}&apiKey=${params.apiKey}`)
         .then(res => res.json())
         .then(data => {
           commit("FETCH_ARTICLES", data.articles);
+          commit("OFF_LOADER");
           resolve();
         })
         .catch(error => console.log(error))
       })
     },
     fetchSources({ commit }, apiKey) {
+      commit("ON_LOADER");
       return new Promise((resolve, reject) => {
         fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`)
         .then(res => res.json())
         .then(data => {
           commit("FETCH_SOURCES", data.sources);
+          commit("OFF_LOADER");
           resolve();
         })
         .catch(error => console.log(error))
       })
     },
     filterArticles({ commit }, params) {
+      commit("ON_LOADER");
       return new Promise((resolve, reject) => {
         fetch(`https://newsapi.org/v2/top-headlines?sources=${params._source}&apiKey=${params.apiKey}`)
         .then(res => res.json())
         .then(data => {
           commit("FILTER_ARTICLES", data.articles)
+          commit("OFF_LOADER");
         })
         .catch(error => console.log(errors))
       })
