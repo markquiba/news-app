b9f1d1ac81027da4c6cd3ee275f67b0fff1949b5
markquiba <markjquiba@gmail.com>
2018-10-11 05:09:50 +0800

    Add component FilterBySource

---
+++ src/components/FilterBySource.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-select
+    @change="() => filter(selectedSource)"
+    v-model="selectedSource"
+    class="filter-btn"
+    :items="getSourceData"
+    label="Filter by Source"
+    outline
+  ></v-select>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+const apiKey = "099148be22804e849a0c6fe022b7cf5e";
+
+export default {
+  data() {
+    return {
+      selectedSource: ''
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'getSourceData'
+    ])
+  },
+  methods: {
+    filter(selectedSource) {
+      this.$store.dispatch("filterArticles", { _source: selectedSource, apiKey }).then(() => {
+        console.log("Success: Filtered Articles")
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.filter-btn {
+  float: right;
+  padding: 0;
+  margin: 0;
+}
+</style>
