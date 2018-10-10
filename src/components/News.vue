1769c6ce0f49c6920f01b7596c2bca7d33adfdf8
markquiba <markjquiba@gmail.com>
2018-10-10 17:21:56 +0800

    Clean up

--- src/components/News.vue
+++ src/components/News.vue
@@ -37,18 +37,4 @@ export default {
 .top-toolbar {
   overflow: hidden;
 }
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
